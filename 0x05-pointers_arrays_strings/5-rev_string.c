1119912dfe0ae7d30a9952b08a8630263d27d204
Okandeji Sakiru <saveshak33@gmail.com>
2022-12-19 19:22:05 -0800

    reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - Entry point
+ * Description: Reverses a string
+ * @s: Pointer to a char
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int len = strlen(s);
+	int i;
+	char temp;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+}
