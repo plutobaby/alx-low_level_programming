9aed465a9cc9ebfb235c81807c94a11daf89509a
Okandeji Sakiru <saveshak33@gmail.com>
2022-12-19 18:47:00 -0800

    swap values of integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - Entry point
+ * Description: Swaps the value of two integers
+ * @a: Integer one
+ * @b: Integer two
+ *
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
